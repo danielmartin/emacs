d012f86a036444b9803a630575e56acba48cfe79
Yuan Fu <casouri@gmail.com>
2021-08-08 00:24:07 -0500

    checkpoint 9
    - Add font-lock intergration
    - font-lock-tree-sitter-defaults
    - font-lock-tree-sitter-settings

--- src/print.c
+++ src/print.c
@@ -1865,10 +1865,7 @@ print_vectorlike (Lisp_Object obj, Lisp_Object printcharfun, bool escapeflag,
       if (NILP (name))
 	print_c_string ("anonymous", printcharfun);
       else
-	{
-	  eassert (STRINGP (name));
 	  print_string (name, printcharfun);
-	}
       print_c_string (" in ", printcharfun);
       print_string (BVAR (XTS_PARSER (obj)->buffer, name), printcharfun);
       printchar ('>', printcharfun);
